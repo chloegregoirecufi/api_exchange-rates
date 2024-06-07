a3a8eb4cb4fdbc78842e43122a174933c9d8ea86
cloclo <cloclo@cloclo>
2024-06-07 16:18:24 +0200

    create model file

---
+++ api_sql/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy import  Column, Float, Integer, String, Date
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class exchangeRate(Base):
+    __tablename__ = "exchange_rate"
+
+    id = Column(Integer, primary_key=True)
+    date = Column(Date)
+    currency = Column(String)
+    rate = Column(Float)
